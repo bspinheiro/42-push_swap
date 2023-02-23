d17868c96f13c318f386043670c5837c60502ecb
bda-silv <bda-silv@student.42.rio>
2023-02-23 15:50:03 -0300

    WIP : intermediate checkpoint

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: bda-silv <bda-silv@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 21:05:16 by bda-silv          #+#    #+#             */
-/*   Updated: 2023/02/23 15:35:14 by bda-silv         ###   ########.fr       */
+/*   Updated: 2023/02/23 15:50:00 by bda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,21 +23,18 @@
  *
  *
  * */
-static char **p_check(char **pp)
+int	p_check(char **pp)
 {
-	int i = 0;
 	int s1 = ft_printf("%p", &pp[0][0]);
 	int s2 = ft_printf("%x", pp[0]);
 
-	while(pp[++i])
-		;
-	ft_printf("**pp    %p\n", pp);
-	ft_printf("**pp[0] %p\n", &pp[0]);
-	ft_printf("**pp[0][0] %p\n", &pp[0][0]);
-	ft_printf("**pp[0] %x\n", pp[0]);
-	ft_printf("**s1 %i\n", s1);
-	ft_printf("**s2 %i\n", s2);
-	return (0);
+	//ft_printf("**pp    %p\n", pp);
+	//ft_printf("**pp[0] %p\n", &pp[0]);
+	//ft_printf("**pp[0][0] %p\n", &pp[0][0]);
+	//ft_printf("**pp[0] %x\n", pp[0]);
+	//ft_printf("**s1 %i\n", s1);
+	//ft_printf("**s2 %i\n", s2);
+	return (2 + s2 - s1);
 }
 
 static char	**free_all(char **pp)
@@ -49,10 +46,9 @@ static char	**free_all(char **pp)
 	n = 0;
 	if (pp == NULL)
 		return(0);
-	p_check(pp);
 	while (pp[++n])
 		;
-	while (i != n)
+	while (i != n && p_ckeck != 0)
 	{
 		free(pp[i]);
 		pp[i] = (void *) 0;
