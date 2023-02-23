c73d26b631b59c6d5f37d0f1a211f4aa5efde1c4
bda-silv <bda-silv@student.42.rio>
2023-02-23 13:32:26 -0300

    WIP : intermediate checkpoint

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: bda-silv <bda-silv@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 21:05:16 by bda-silv          #+#    #+#             */
-/*   Updated: 2023/02/23 13:10:04 by bda-silv         ###   ########.fr       */
+/*   Updated: 2023/02/23 13:32:22 by bda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ static char	**free_all(char **pp)
 		return(0);
 	while (pp[++n])
 		;
-	while (n != 0 && i != n && pp != NULL)
+	while (i != n && pp != NULL && pp[i] != NULL)
 	{
 		free(pp[i]);
 		pp[i] = (void *) 0;
