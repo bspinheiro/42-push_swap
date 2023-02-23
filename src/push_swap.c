7578cdc2aefe77a11723d310d4098446abc9d851
bda-silv <bda-silv@student.42.rio>
2023-02-23 16:32:25 -0300

    WIP : intermediate checkpoint

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: bda-silv <bda-silv@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 21:05:16 by bda-silv          #+#    #+#             */
-/*   Updated: 2023/02/23 16:26:09 by bda-silv         ###   ########.fr       */
+/*   Updated: 2023/02/23 16:32:18 by bda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,15 +25,17 @@
  * */
 int	p_check(char **pp)
 {
-	char *p1 = &pp[0][0];
-	char *p2 = pp[0];
+	//char **p1 = pp[0][0];
+	//char **p2 = &pp[0];
 
 	//ft_printf("**pp    %p\n", pp);
 	//ft_printf("**pp[0] %p\n", &pp[0]);
-	//ft_printf("**pp[0][0] %p\n", &pp[0][0]);
-	//ft_printf("**pp[0] %x\n", pp[0]);
-	ft_printf("**p1 %s\n", p1);
-	ft_printf("**p2 %s\n", p2);
+	ft_printf("**pp[0][0] %p\n", &pp[0][0]);
+	ft_printf("**pp[0] %x\n", pp[0]);
+	ft_printf("sizeof pp[0][0] %i\n", sizeof(&pp[0][0]));
+	ft_printf("sizeof pp[0] %i\n", sizeof(pp[0]));
+	//ft_printf("**p1 %s\n", p1);
+	//ft_printf("**p2 %s\n", p2);
 	return (0);
 }
 
