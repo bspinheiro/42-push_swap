6a0eb1aa4ef1ff948a16e0b5ebad7ef5a7cbd09e
bda-silv <bda-silv@student.42.rio>
2023-02-23 14:06:53 -0300

    WIP : intermediate checkpoint

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: bda-silv <bda-silv@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 21:05:16 by bda-silv          #+#    #+#             */
-/*   Updated: 2023/02/23 14:06:21 by bda-silv         ###   ########.fr       */
+/*   Updated: 2023/02/23 14:06:50 by bda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ static char **p_check(char **pp)
 		;
 	ft_printf("==> n=%i\n", n);
 	ft_printf("**pp     %p\n", pp);
-	while (++i != n)
+	while (i++ != n)
 		ft_printf("**pp[%i] %p\n", i, pp[i]);
 	return (0);
 }
